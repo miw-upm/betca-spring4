0b9534feebf20d0d5395a1f6982dfe27863da8de
jbernal <j.bernal@upm.es>
2018-01-18 01:47:30 +0100

    Add Swagger

---
+++ src/main/java/miw/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package miw.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build()
+                .apiInfo(apiInfo());
+    }
+
+    //http://localhost:8080/swagger-ui.html
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Máster en Ingeniería Web. Universidad Politécnica de Madrid")
+                .description("BETCA. Back-end con Tecnologías de Código Abierto (SPRING). https://github.com/miw-upm/BETCA-spring").build();
+    }
+
+}
