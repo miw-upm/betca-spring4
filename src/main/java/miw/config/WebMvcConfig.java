7edc0d76e29e89708f45e8db6b6a0b8ba1367b99
jbernal <j.bernal@upm.es>
2018-01-19 06:42:26 +0100

    Update default page

--- src/main/java/miw/config/WebMvcConfig.java
+++ src/main/java/miw/config/WebMvcConfig.java
@@ -1,8 +1,10 @@
 package miw.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import miw.miscellaneous.TimeBasedAccessInterceptor;
@@ -20,6 +22,13 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new TimeBasedAccessInterceptor()).addPathPatterns(AdminResource.ADMINS + AdminResource.OUT_OF_TIME + "/**")
                 .excludePathPatterns("/foo/**");
+    }
+    
+    @Override
+    public void addViewControllers( ViewControllerRegistry registry ) {
+        registry.addViewController( "/" ).setViewName( "forward:/swagger-ui.html" );
+        registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
+        super.addViewControllers( registry );
     }
 
 }
