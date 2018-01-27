a7928fe5c487f74d0722b8c7e60f5471e9f45ce6
jbernal <j.bernal@upm.es>
2018-01-27 19:21:46 +0100

    Clean Swagger

--- src/main/java/miw/config/SwaggerConfig.java
+++ src/main/java/miw/config/SwaggerConfig.java
@@ -17,7 +17,11 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build()
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
                 .apiInfo(apiInfo());
     }
 
