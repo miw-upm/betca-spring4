872e44bd7d95a3108ad88a8563779c130cd5b2d1
jbernal <j.bernal@upm.es>
2018-03-03 18:26:13 +0100

    Refactoring

--- src/main/java/miw/persistence/mongo/MongoConfiguration.java
+++
@@ -1,7 +0,0 @@
-package miw.persistence.mongo;
-
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-@EnableMongoRepositories
-public interface MongoConfiguration {
-}
